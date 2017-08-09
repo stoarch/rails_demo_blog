0e8862a7afb3c129102abc6f6d7b5678a6686477
stoarch <stormarchitextor@gmail.com>
2017-08-09 12:39:19 +0100

    Errors on main screen visible; Fixtures for user->author relation is added

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base
   has_many :posts
   has_many :comments
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809110150) do
+ActiveRecord::Schema.define(version: 20170809113028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170809110150) do
     t.date     "birth_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
